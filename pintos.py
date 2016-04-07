50f6df916b27b28ed318b533d1b91e53afce84e2
Fredrik Jonsén <fredrikjonsen@gmail.com>
2016-04-07 18:06:01 +0200

    Moved script to base of project

--- pintos.py
+++ pintos.py
@@ -2,21 +2,36 @@
 
 import subprocess
 import argparse
+from os import getcwd
+from os.path import basename
+
+minpath = "pintos/src/userprog"
+
+cwd = basename(getcwd())
+
+if cwd != "userprog":
+    pos = minpath.find(cwd)
+    movepath = minpath[minpath.find("/", pos):]
+    from os import chdir
+    chdir(getcwd() + movepath)
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", action="store_true", help="Runs Make before user program")
 parser.add_argument("-v", action="store_true", help="Run user terminal")
+parser.add_argument("-u", action="store_true", help="Run Make on examples")
 parser.add_argument("program", help="user program to run")
 parser.add_argument("programargs", help="optional arguments for user program", default=[], nargs=argparse.REMAINDER)
 
 args = parser.parse_args()
 
-if args.m:
-    try:
-        subprocess.call("make")
-    except ErrorReturnCode:
-        print("Make failed, exiting")
-        exit(1)
+if args.m and subprocess.call("make", shell=True) != 0:
+    print("Make failed, exiting")
+    exit(1)
+
+if args.u and subprocess.call("make -C ../examples", shell=True) != 0:
+    print("Make Examples failed, exiting")
+    exit(1)
 
 command = "pintos -p ../examples/{0} -a {0}"
 
